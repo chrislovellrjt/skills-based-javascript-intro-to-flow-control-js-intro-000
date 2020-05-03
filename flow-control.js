c2c38c12006d44fc3cb74e2a90858aff8fa6a23c
Chris Lovell <chrislovell_1@hotmail.com>
2020-05-03 20:58:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,6 +25,11 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
+  if (age > 12 && age <20) {
+    return "You are a teenager";
+  } return {
+    "You are not a teenager";
+  }
 
 }
 
